84998777cd61e4531fbf4abdf9e95b7078e22d13
Tomas Carignano <tomascarignano98@gmail.com>
2021-10-05 14:07:34 -0300

    Added state to the textarea.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
   return (
     <>
       <h1>Title</h1>
-      <textarea value="Text area to type in" />
+      <textarea
+        placeholder="Text area to type in"
+        value={text}
+        onChange={handleChange}
+      />
       <h2>Time remaining</h2>
       <button>Start Game</button>
       <h2>Word count</h2>
