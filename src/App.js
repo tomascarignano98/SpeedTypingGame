9b128c95ce6100d395abad135b4aab5083cc13df
Tomas Carignano <tomascarignano98@gmail.com>
2021-10-05 14:42:43 -0300

    Added countdown timer with useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [text, setText] = useState('');
+
+  const [timeLeft, setTimeLeft] = useState(3);
+  useEffect(() => {
+    if (timeLeft > 0) {
+      const timerId = setTimeout(() => {
+        setTimeLeft((prev) => prev - 1);
+      }, 1000);
+
+      return () => clearTimeout(timerId);
+    }
+  }, [timeLeft]);
 
   function handleChange(event) {
     setText(event.target.value);
@@ -22,7 +33,7 @@ function App() {
         value={text}
         onChange={handleChange}
       />
-      <h2>Time remaining</h2>
+      <h2>Time remaining: {timeLeft}</h2>
       <button onClick={() => calculateWordCount(text)}>Start Game</button>
       <h2>Word count</h2>
     </>
