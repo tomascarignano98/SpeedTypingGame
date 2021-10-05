d406e9cd00951f668689061bd81425a58350386b
Tomas Carignano <tomascarignano98@gmail.com>
2021-10-05 11:10:48 -0300

    Built initial UI.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Title</h1>
+      <textarea value="Text area to type in" />
+      <h2>Time remaining</h2>
+      <button>Start Game</button>
+      <h2>Word count</h2>
+    </>
   );
 }
 
